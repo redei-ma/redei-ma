f2f237461e3708d233ab9a2b783aaedf3365ec17
redei-ma <renato.deimarini@gmail.com>
2024-12-05 12:09:57 +0100

    Update get_next_line_bonus.h

--- CommonCore/get_next_line/get_next_line_bonus.h
+++ CommonCore/get_next_line/get_next_line_bonus.h
@@ -13,15 +13,15 @@
 #ifndef GET_NEXT_LINE_BONUS_H
 # define GET_NEXT_LINE_BONUS_H
 
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
+
 # include <stdlib.h>
 # include <unistd.h>
 # include <fcntl.h>
 # include <limits.h>
 # include <stdio.h>
-
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 5
-# endif
 
 char	*find_newline(char **str);
 int		read_line(char **s, int fd);
